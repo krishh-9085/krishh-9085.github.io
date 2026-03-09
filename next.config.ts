f063b9ed095c4a1c11492674241fdf0306dfe38e
Krishna Rohilla <rohillakrish2@gmail.com>
2026-03-10 02:16:19 +0530

    Configure basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS || false;
+const repoName = "Next-Portfolio";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  ...(isGithubPages && {
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}/`,
+  }),
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
